d0309df976e03ba85a08fc239022223be87e9002
mergimuka-psih <mergimuka1@gmail.com>
2020-07-19 20:42:33 +0200

    first commit

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -21,11 +21,11 @@ class TestCase extends \Orchestra\Testbench\TestCase
   protected function getEnvironmentSetUp($app)
   {
      // Setup default database to use sqlite :memory:
-    //  $app['config']->set('database.default', 'testbench');
-    //  $app['config']->set('database.connections.testbench', [
-    //      'driver'   => 'sqlite',
-    //      'database' => ':memory:',
-    //      'prefix'   => '',
-    //  ]);
+     $app['config']->set('database.default', 'testbench');
+     $app['config']->set('database.connections.testbench', [
+         'driver'   => 'sqlite',
+         'database' => ':memory:',
+         'prefix'   => '',
+     ]);
   }
 }
